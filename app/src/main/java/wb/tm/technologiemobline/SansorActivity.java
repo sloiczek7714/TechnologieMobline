a6eba1b26357ff344ef8e3335f1865961cc4513c
Weronika-Buras-WAT <weronika.buras@student.wat.edu.pl>
2022-11-15 10:03:24 +0100

    Initial commit

---
+++ app/src/main/java/wb/tm/technologiemobline/SansorActivity.java
@@ -0,0 +1,14 @@
+package wb.tm.technologiemobline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SansorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sansor);
+    }
+}
